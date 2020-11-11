e20bb590ce2b468b493f854a74ebf6fb8f66bf5c
yesminee <43929137+yesminee@users.noreply.github.com>
2020-11-11 01:39:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const app = express();
 const router = require("./routes/person");
-
 const connectDB = require("./config/connectDB");
 
 //middleware
 app.use(express.json());
-
 app.use("/persons", router);
-
 
 // Connection database
 connectDB();
